236c07bcd592c6009efd9740d33d55ec7b2e8849
MarianoMushy <tachiangel27@gmail.com>
2022-10-07 09:58:02 +0200

    Se agrego el Coyote time y jump buffer

--- UnityProyecto3D-Atzuh,Bluegx,Muchi/Assets/Scripts/ThirdPersonController.cs
+++ UnityProyecto3D-Atzuh,Bluegx,Muchi/Assets/Scripts/ThirdPersonController.cs
@@ -28,14 +28,39 @@ public class ThirdPersonController : MonoBehaviour
 
     public float salto = 1;
 
+    private float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+
+    private float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
+
 
     private void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
+        }
+
+
+        if ((isGrounded))
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
         }
 
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -54,12 +79,17 @@ public class ThirdPersonController : MonoBehaviour
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (jumpBufferCounter > 0f && coyoteTimeCounter > 0f)
         {
             Jump(salto);
+            jumpBufferCounter = 0;
         }
 
-
+        if (Input.GetButtonUp("Jump"))
+        {
+            coyoteTimeCounter = 0f;
+        }
+        
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
